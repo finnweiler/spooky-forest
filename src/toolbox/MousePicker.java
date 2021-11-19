0ba28895d9c822bc23cea7c55a3fc533f29640d9
Niklas <dev@corusm.de>
2021-11-19 12:50:29 +0100

    add mousepicker comments

--- src/toolbox/MousePicker.java
+++ src/toolbox/MousePicker.java
@@ -1,12 +1,13 @@
 package toolbox;
 
-import entities.Camera;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Matrix4f;
 import org.lwjgl.util.vector.Vector2f;
 import org.lwjgl.util.vector.Vector3f;
 import org.lwjgl.util.vector.Vector4f;
+
 import terrain.Terrain;
+import entities.Camera;
 
 public class MousePicker {
 
@@ -15,59 +16,80 @@ public class MousePicker {
 
     private Vector3f currentRay = new Vector3f();
 
-    private final Matrix4f projectionMatrix;
-    private final Camera camera;
+    private Matrix4f projectionMatrix;
+    private Matrix4f viewMatrix;
+    private Camera camera;
 
-    private final Terrain terrain;
+    private Terrain terrain;
     private Vector3f currentTerrainPoint;
 
+    // Constructor Mousepicker
+    // Kriegt Kamera, Pojektion und terrain
     public MousePicker(Camera cam, Matrix4f projection, Terrain terrain) {
         camera = cam;
         projectionMatrix = projection;
+        viewMatrix = Maths.createViewMatrix(camera);
         this.terrain = terrain;
     }
 
+    // Getter für current terrain point
     public Vector3f getCurrentTerrainPoint() {
         return currentTerrainPoint;
     }
 
+    // getter for current ray (strahl)
     public Vector3f getCurrentRay() {
         return currentRay;
     }
 
+    // Mouseray errechnen
+    // Intersection mit teraiin suchen
+    // Punkt auf terrrain in currentterrainpounkt speichern
     public void update() {
+        viewMatrix = Maths.createViewMatrix(camera);
         currentRay = calculateMouseRay();
-        currentTerrainPoint = intersectionInRange(0, RAY_RANGE, currentRay) ? binarySearch(0, 0, RAY_RANGE, currentRay) : null;
+        if (intersectionInRange(0, RAY_RANGE, currentRay)) {
+            currentTerrainPoint = binarySearch(0, 0, RAY_RANGE, currentRay);
+        } else {
+            currentTerrainPoint = null;
+        }
     }
 
+    // Maus Ray berechnen (Schritt 1)
     private Vector3f calculateMouseRay() {
-        float mouseX = (float) Display.getWidth() / 2;
-        float mouseY = (float) Display.getHeight() / 2;
+        float mouseX = Display.getWidth() / 2;
+        float mouseY = Display.getHeight() / 2;
         Vector2f normalizedCoords = getNormalisedDeviceCoordinates(mouseX, mouseY);
         Vector4f clipCoords = new Vector4f(normalizedCoords.x, normalizedCoords.y, -1.0f, 1.0f);
         Vector4f eyeCoords = toEyeCoords(clipCoords);
-        return toWorldCoords(eyeCoords);
+        Vector3f worldRay = toWorldCoords(eyeCoords);
+        return worldRay;
     }
 
-    private Vector3f toWorldCoords(Vector4f eyeCoords) {
-        Matrix4f invertedView = Matrix4f.invert(Maths.createViewMatrix(camera), null);
-        Vector4f rayWorld = Matrix4f.transform(invertedView, eyeCoords, null);
-        Vector3f mouseRay = new Vector3f(rayWorld.x, rayWorld.y, rayWorld.z);
-        mouseRay.normalise();
-        return mouseRay;
+    // Normalized Device Space (Schritt 2)
+    private Vector2f getNormalisedDeviceCoordinates(float mouseX, float mouseY) {
+        float x = (2.0f * mouseX) / Display.getWidth() - 1f;
+        float y = (2.0f * mouseY) / Display.getHeight() - 1f;
+        return new Vector2f(x, y);
     }
 
+    // In EyeCoords umwandeln (Schritt 3)
     private Vector4f toEyeCoords(Vector4f clipCoords) {
         Matrix4f invertedProjection = Matrix4f.invert(projectionMatrix, null);
         Vector4f eyeCoords = Matrix4f.transform(invertedProjection, clipCoords, null);
         return new Vector4f(eyeCoords.x, eyeCoords.y, -1f, 0f);
     }
 
-    private Vector2f getNormalisedDeviceCoordinates(float mouseX, float mouseY) {
-        float x = (2.0f * mouseX) / Display.getWidth() - 1f;
-        float y = (2.0f * mouseY) / Display.getHeight() - 1f;
-        return new Vector2f(x, y);
+    // In Weltkoordinaten umwandeln (Schritt 3)
+    private Vector3f toWorldCoords(Vector4f eyeCoords) {
+        Matrix4f invertedView = Matrix4f.invert(viewMatrix, null);
+        Vector4f rayWorld = Matrix4f.transform(invertedView, eyeCoords, null);
+        Vector3f mouseRay = new Vector3f(rayWorld.x, rayWorld.y, rayWorld.z);
+        mouseRay.normalise();
+        return mouseRay;
     }
+
+    //**********************************************************
 
     private Vector3f getPointOnRay(Vector3f ray, float distance) {
         Vector3f camPos = camera.getPosition();
@@ -76,11 +98,12 @@ public class MousePicker {
         return Vector3f.add(start, scaledRay, null);
     }
 
+    // Punkt auf Terrain zurückgeben
     private Vector3f binarySearch(int count, float start, float finish, Vector3f ray) {
         float half = start + ((finish - start) / 2f);
         if (count >= RECURSION_COUNT) {
             Vector3f endPoint = getPointOnRay(ray, half);
-            Terrain terrain = getTerrain();
+            Terrain terrain = getTerrain(endPoint.getX(), endPoint.getZ());
             if (terrain != null) {
                 return endPoint;
             } else {
@@ -94,22 +117,33 @@ public class MousePicker {
         }
     }
 
+    // Sagen ob punkt von Ray unter terrain ist
     private boolean intersectionInRange(float start, float finish, Vector3f ray) {
         Vector3f startPoint = getPointOnRay(ray, start);
         Vector3f endPoint = getPointOnRay(ray, finish);
-        return !isUnderGround(startPoint) && isUnderGround(endPoint);
+        if (!isUnderGround(startPoint) && isUnderGround(endPoint)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
+    // Test if ray point is underground
     private boolean isUnderGround(Vector3f testPoint) {
-        Terrain terrain = getTerrain();
+        Terrain terrain = getTerrain(testPoint.getX(), testPoint.getZ());
         float height = 0;
         if (terrain != null) {
             height = terrain.getHeight(testPoint.getX(), testPoint.getZ());
         }
-        return testPoint.y < height;
+        if (testPoint.y < height) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    private Terrain getTerrain() {
+    // Terrain Objekt zurückgeben
+    private Terrain getTerrain(float worldX, float worldZ) {
         return terrain;
     }
 }
